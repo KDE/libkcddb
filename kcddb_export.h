d076fb0eba2d81ef99b701ba5f55587bec7e3081
Laurent Montel <montel@kde.org>
2007-08-11 10:01:20 +0000

    Update export file
    svn path=/trunk/KDE/kdemultimedia/libkcddb/; revision=698864

--- kcddb_export.h
+++ kcddb_export.h
@@ -1,6 +1,5 @@
-/*
-    This file is part of libkcddb.
-    Copyright (c) 2006 Laurent Montel <montel@kde.org>
+/*  This file is part of the KDE project
+    Copyright (C) 2007 David Faure <faure@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -21,23 +20,21 @@
 #ifndef KCDDB_EXPORT_H
 #define KCDDB_EXPORT_H
 
-/* needed for KDE_EXPORT macros */
+/* needed for KDE_EXPORT and KDE_IMPORT macros */
 #include <kdemacros.h>
 
-#if defined Q_OS_WIN
 #ifndef KCDDB_EXPORT
-# ifdef MAKE_KCDDB_LIB
+# if defined(MAKE_KCDDB_LIB)
+   /* We are building this library */ 
 #  define KCDDB_EXPORT KDE_EXPORT
 # else
+   /* We are using this library */ 
 #  define KCDDB_EXPORT KDE_IMPORT
 # endif
 #endif
 
-#else /* UNIX*/
-
-/* export statements for unix */
-#define KCDDB_EXPORT KDE_EXPORT
-
-#endif
+# ifndef KCDDB_EXPORT_DEPRECATED
+#  define KCDDB_EXPORT_DEPRECATED KDE_DEPRECATED KCDDB_EXPORT
+# endif
 
 #endif
