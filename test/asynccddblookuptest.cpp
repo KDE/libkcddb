a090534609a55fb890cc53820b119777f59a2d9f
David Faure <faure@kde.org>
2009-05-22 15:10:58 +0000

    Update cddb data for the CD being tested by this unittest. Next test failure is musicbrainztest, lookup doesn't return Success... CCMAIL: nouseforaname@home.se
    svn path=/trunk/KDE/kdemultimedia/libkcddb/; revision=971509

--- test/asynccddblookuptest.cpp
+++ test/asynccddblookuptest.cpp
@@ -63,32 +63,32 @@ void AsyncCDDBLookupTest::testLookup()
 
   // If revision doesn't match, test probably needs to be updated
   // See: http://www.freedb.org/freedb/jazz/a1107d0a for updated data
-  QCOMPARE(m_info.get("revision").toInt(), 4);
+  QCOMPARE(m_info.get("revision").toInt(), 5);
 
   QCOMPARE(m_info.get(Artist).toString(),QString("Kruder & Dorfmeister"));
-  QCOMPARE(m_info.get(Title).toString(),QString("K&D Sessions"));
+  QCOMPARE(m_info.get(Title).toString(),QString("The K&D Sessions - CD1"));
   QCOMPARE(m_info.get(Genre).toString(),QString("Tribal"));
   QCOMPARE(m_info.get(Year).toInt(),1998);
-  QCOMPARE(m_info.track(0).get(Title).toString(),QString("Heroes [kruder's long loose bossa]"));
-  QCOMPARE(m_info.track(1).get(Title).toString(),QString("Jazz Master [k&d session]"));
-  QCOMPARE(m_info.track(2).get(Title).toString(),QString("Speechless [drum 'n' bass]"));
-  QCOMPARE(m_info.track(3).get(Title).toString(),QString("Going Under [main version - k&d session]"));
-  QCOMPARE(m_info.track(4).get(Title).toString(),QString("Bug Powder Dust [k&d session]"));
-  QCOMPARE(m_info.track(5).get(Title).toString(),QString("Rollin' On Chrome [wild motherfucker dub]"));
-  QCOMPARE(m_info.track(6).get(Title).toString(),QString("Useless [k&d session]"));
-  QCOMPARE(m_info.track(7).get(Title).toString(),QString("Gotta Jazz [dorfmeister remix]"));
-  QCOMPARE(m_info.track(8).get(Title).toString(),QString::fromUtf8("Donaueschingen [peter kruder's donaudampfschifffahrtsgesellschaftskapitänskajütenremix]"));
-  QCOMPARE(m_info.track(9).get(Title).toString(),QString("Trans Fatty Acid [k&d session]"));
-  QCOMPARE(m_info.track(0).get(Artist).toString(),QString("Roni Size"));
-  QCOMPARE(m_info.track(1).get(Artist).toString(),QString("Alex Reece"));
-  QCOMPARE(m_info.track(2).get(Artist).toString(),QString("Count Basic"));
-  QCOMPARE(m_info.track(3).get(Artist).toString(),QString("Rocker's HiFi"));
-  QCOMPARE(m_info.track(4).get(Artist).toString(),QString("Bomb The Bass"));
-  QCOMPARE(m_info.track(5).get(Artist).toString(),QString("Aphrodelics"));
-  QCOMPARE(m_info.track(6).get(Artist).toString(),QString("Depeche Mode"));
-  QCOMPARE(m_info.track(7).get(Artist).toString(),QString("Count Basic"));
-  QCOMPARE(m_info.track(8).get(Artist).toString(),QString::fromUtf8("Trüby Trio"));
-  QCOMPARE(m_info.track(9).get(Artist).toString(),QString("Lamb"));
+  QCOMPARE(m_info.track(0).get(Title).toString(),QString("Roni Size - Heroes (Kruder's Long Loose Bossa)"));
+  QCOMPARE(m_info.track(1).get(Title).toString(),QString("Alex Reece - Jazz Master (K&D Session)"));
+  QCOMPARE(m_info.track(2).get(Title).toString(),QString("Count Basic - Speechless (Drum 'N' Bass)"));
+  QCOMPARE(m_info.track(3).get(Title).toString(),QString("Rocker's HiFi - Going Under (Main Version - K&D Session)"));
+  QCOMPARE(m_info.track(4).get(Title).toString(),QString("Bomb The Bass - Bug Powder Dust (K&D Session)"));
+  QCOMPARE(m_info.track(5).get(Title).toString(),QString("Aphrodelics - Rollin' On Chrome (Wild Motherfucker Dub)"));
+  QCOMPARE(m_info.track(6).get(Title).toString(),QString("Depeche Mode - Useless (K&D Session)"));
+  QCOMPARE(m_info.track(7).get(Title).toString(),QString("Count Basic - Gotta Jazz (Dorfmeister Remix)"));
+  QCOMPARE(m_info.track(8).get(Title).toString(),QString::fromUtf8("Trüby Trio - Donaueschingen (Peter Kruder's Donaudampfschifffahrtsgesellschaftskapitaenskajuetenremix)"));
+  QCOMPARE(m_info.track(9).get(Title).toString(),QString("Lamb - Trans Fatty Acid (K&D Session)"));
+  QCOMPARE(m_info.track(0).get(Artist).toString(),QString("Kruder & Dorfmeister"));
+  QCOMPARE(m_info.track(1).get(Artist).toString(),QString("Kruder & Dorfmeister"));
+  QCOMPARE(m_info.track(2).get(Artist).toString(),QString("Kruder & Dorfmeister"));
+  QCOMPARE(m_info.track(3).get(Artist).toString(),QString("Kruder & Dorfmeister"));
+  QCOMPARE(m_info.track(4).get(Artist).toString(),QString("Kruder & Dorfmeister"));
+  QCOMPARE(m_info.track(5).get(Artist).toString(),QString("Kruder & Dorfmeister"));
+  QCOMPARE(m_info.track(6).get(Artist).toString(),QString("Kruder & Dorfmeister"));
+  QCOMPARE(m_info.track(7).get(Artist).toString(),QString("Kruder & Dorfmeister"));
+  QCOMPARE(m_info.track(8).get(Artist).toString(),QString("Kruder & Dorfmeister"));
+  QCOMPARE(m_info.track(9).get(Artist).toString(),QString("Kruder & Dorfmeister"));
 }
 
   void
