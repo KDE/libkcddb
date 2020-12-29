c6c2f63ee294bb4bdeee399fdd11c6ec6eb1bae3
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-29 09:21:23 +0100

    Use ecm_qt_declare_logging_category, install libkcddb.categories
    GIT_SILENT

--- libkcddb/logging.cpp
+++
@@ -1,9 +0,0 @@
-/*
-    SPDX-FileCopyrightText: 2003-2004 Richard Lärkäng <nouseforaname@home.se>
-
-    SPDX-License-Identifier: LGPL-2.0-or-later
-*/
-
-#include "logging.h"
-
-Q_LOGGING_CATEGORY(LIBKCDDB, "libkcddb")
